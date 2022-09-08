9870a0dd8aea68829c76b93c05a7e94d6c1eaffc
shiwoo.park <custardcream@kakao.com>
2022-09-09 03:19:45 +0900

    Feature: jQuery 이용 기능 구현

--- js/10000hours.js
+++ js/10000hours.js
@@ -1,1 +1,42 @@
-// js file
+$(function () {
+  $("#target-form").submit(getAndShowResults);
+  $("#modal-opener").click(openModal);
+  $(".modal-wrapper").click(closeModal);
+});
+
+function getAndShowResults(event) {
+  const target = $("#target").val();
+  const hours = $("#hours-for-target").val();
+
+  if (target === "" || isNaN(hours)) {
+    event.preventDefault();
+    return;
+  }
+
+  $("#target-result").text(target);
+  $("#days-for-target").text(calDaysForTarget(hours));
+  $("html").animate(
+    {
+      scrollTop: $("#target-result").offset().top,
+    },
+    600
+  );
+
+  event.preventDefault();
+}
+
+function calDaysForTarget(hoursPerDay) {
+  return Math.round(10000 / hoursPerDay).toString();
+}
+
+function openModal() {
+  $(".modal-wrapper").css("display", "block");
+  $(".modal-wrapper").animate({ opacity: 0 }, 0);
+  $(".modal-wrapper").animate({ opacity: 1 }, 150);
+}
+
+function closeModal() {
+  $(".modal-wrapper").animate({ opacity: 0 }, 150, () =>
+    $(".modal-wrapper").css("display", "none")
+  );
+}
