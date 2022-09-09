d5870f1e337ce53dc62396360c45a0554f10378e
shiwoo.park <custardcream@kakao.com>
2022-09-09 15:21:31 +0900

    Chore: Cleann Code & 주석 추가

--- js/10000hours.js
+++ js/10000hours.js
@@ -1,45 +1,126 @@
-const $form = document.getElementById("target-form");
-const $target = document.getElementById("target");
-const $hours = document.getElementById("hours-for-target");
-const $targetResult = document.getElementById("target-result");
-const $resultValue = document.getElementById("days-for-target");
+/*
+  Vanilla JS
 
-$form.addEventListener("submit", getAndShowResults);
+  공통 로직
 
-const $modalOpener = document.getElementById("modal-opener");
-const $modalWrapper = document.getElementById("modal-wrapper");
+  1. 처음 페이지가 로드될 때 eventListener들을 선언합니다.
 
-$modalOpener.addEventListener("click", openModal);
+  각각의 로직은 각 함수에 적었습니다.
+*/
+
+const $ruleForm = document.getElementsByClassName("rule-form-js")[0];
+const $inputTarget = document.getElementsByClassName(
+  "rule-form__input-target-js"
+)[0];
+const $inputHours = document.getElementsByClassName(
+  "rule-form__input-hours-js"
+)[0];
+const $resultTarget = document.getElementsByClassName("result__target-js")[0];
+const $resultDays = document.getElementsByClassName("result__days-js")[0];
+
+const $shareBtn = document.getElementsByClassName("share-link")[0];
+
+const $modal = document.getElementsByClassName("cheerup-modal-js")[0];
+const $modalOpenerBtn = document.getElementsByClassName(
+  "cheerup-modal__opener-js"
+)[0];
+const $modalWrapper = document.getElementsByClassName(
+  "cheerup-modal__wrapper-js"
+)[0];
+
+$ruleForm.addEventListener("submit", getAndShowResults);
+
+$shareBtn.addEventListener("click", copyShareLink);
+
+$modalOpenerBtn.addEventListener("click", openModal);
 $modalWrapper.addEventListener("click", closeModal);
 
+/**
+ * 입력값이 들어오는 `<form>` 요소의
+ * `submit` 이벤트를 헨들링하는 함수
+ * @param {SubmitEvent} event
+ * @returns
+ */
 function getAndShowResults(event) {
-  const targetVal = $target.value;
-  const hoursVal = $hours.value;
-  console.log(targetVal);
+  const targetVal = $inputTarget.value;
+  const hoursVal = parseFloat($inputHours.value);
+  // 각 입력값을 불러옵니다.
+  // 시간은 float으로 형변환합니다.
 
   if (targetVal === "" || isNaN(hoursVal)) {
+    // 두 값중 하나라도 비어있다면 아무것도 하지 않습니다.
     event.preventDefault();
     return;
   }
 
-  $targetResult.textContent = targetVal;
-  $resultValue.textContent = calDaysForTarget(hoursVal);
+  // 결과가 들어갈 곳을 채웁니다.
+  $resultTarget.textContent = targetVal;
+  $resultDays.textContent = calDaysForTarget(hoursVal);
 
-  $targetResult.scrollIntoView({ behavior: "smooth" });
+  // 결과로 자동 스크롤
+  $resultTarget.scrollIntoView({ behavior: "smooth" });
 
+  // default action의 작동을 막습니다.
   event.preventDefault();
 }
 
+/**
+ * 1만 시간까지 몇 일 걸릴지 계산하는 함수
+ * @param {number} hoursPerDay - 하루에 투자할 시간
+ * @returns {string} 1만 시간까지 걸리는 일 수
+ */
 function calDaysForTarget(hoursPerDay) {
   return Math.round(10000 / hoursPerDay).toString();
 }
 
+/**
+ * 공유할 링크를
+ * 사용자의 클립보드에 복사하는 함수
+ *
+ * 복사에는 `Clipboard API`를 사용했습니다.
+ *
+ * [참고한 MDN 문서](https://developer.mozilla.org/en-US/docs/Web/API/Clipboard_API)
+ * @param {MouseEvent} event
+ * @return void
+ */
+async function copyShareLink(event) {
+  // event를 일으킨 요소에서 data attribute를 통해 복사할 링크를 가져옵니다.
+  const link = event.target.dataset.copiedlink;
+
+  // writeText 메소드는 비동기적으로 작동합니다.
+  await navigator.clipboard.writeText(link);
+
+  window.alert("🙌 링크가 복사됐습니다 🙌\n친구에게 공유해보세요 😁👍");
+}
+
+/**
+ * modal 창을 여는 함수
+ */
 function openModal() {
+  /*
+    먼저 display: none; 이던 wrapper에
+    display: block;을 부여해 보이도록 합니다.
+  */
   $modalWrapper.style.display = "block";
+
+  /*
+    activated class를 부여해서 animation을 트리거합니다.
+  */
   $modalWrapper.classList.add("activated");
 }
 
+/**
+ * modal 창을 닫는 함수
+ */
 function closeModal() {
+  /*
+    activated class를 삭제해서 animation을 트리거합니다.
+  */
   $modalWrapper.classList.remove("activated");
+
+  /*
+    400ms만큼의 애니메이션이 끝나면
+    display: none;을 다시 부여해 가립니다.
+  */
   setTimeout(() => ($modalWrapper.style.display = "none"), 400);
 }
